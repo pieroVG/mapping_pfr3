2e2e946265abcde834ef19629fd27486d2d3df54
pieroVG <157099501+pieroVG@users.noreply.github.com>
2024-05-17 09:06:37 +0200

    Update commandes_pi.sh

--- commandes_pi.sh
+++ commandes_pi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Lancer les commandes dans différents terminaux en utilisant des sessions SSH
-gnome-terminal -- bash -c "./run_ssh_commands_terminal_1.sh; exec bash"
-gnome-terminal -- bash -c "./run_ssh_commands_terminal_2.sh; exec bash"
+gnome-terminal -- bash -c "./lidar_launch.sh; exec bash"
+gnome-terminal -- bash -c "./camera_launch.sh; exec bash"
 
