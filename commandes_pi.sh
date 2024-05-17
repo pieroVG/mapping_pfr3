a5aac54ad41bdff92e7fba9b4422980d27ba44e5
pieroVG <157099501+pieroVG@users.noreply.github.com>
2024-05-17 09:02:11 +0200

    Update commandes_pi.sh

--- commandes_pi.sh
+++ commandes_pi.sh
@@ -1,32 +1,6 @@
-#!/usr/bin/expect
+#!/bin/bash
 
-# Commandes à exécuter dans la session SSH
-commandes_terminal_1=(
-    "ros2 launch mapping_pfr3 rplidar.launch.py"
-)
+# Lancer les commandes dans différents terminaux en utilisant des sessions SSH
+gnome-terminal -- bash -c "./run_ssh_commands_terminal_1.sh; exec bash"
+gnome-terminal -- bash -c "./run_ssh_commands_terminal_2.sh; exec bash"
 
-commandes_terminal_2=(
-    "ros2 launch mapping_pfr3 camera.launch.py"
-)
-
-# Fonction pour exécuter les commandes dans la session SSH
-proc execute_commands {commands} {
-    foreach command $commands {
-        send "$command\r"
-        expect "$ "
-    }
-}
-
-# Ouvre une session SSH et exécute les commandes_terminal_2
-spawn ssh samuel@192.168.161.145
-expect "password:"
-send "14102002\r"
-expect "$ "
-execute_commands $commandes_terminal_2
-
-# Exécute les commandes_terminal_1 après avoir terminé les commandes_terminal_2
-execute_commands $commandes_terminal_1
-
-# Quitte la session SSH
-send "exit\r"
-expect eof
